71860fc6fe7d17df60568bef6971f8249fdaf326
Pat <pat.barilla@gmail.com>
2024-06-19 22:17:48 +1000

    beep boop

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,37 +6,38 @@ const footer = () => {
       <div
         style={{
           display: "flex",
+          backgroundImage: `url("/GettyImages-869077028-2880w.jpg")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
         }}
       >
         <div style={{ flexDirection: "row" }}>
-          <div>Quick Links</div>
+          <div style={{ marginBottom: 10 }}>Quick Links</div>
           <div>Home</div>
           <div>What we offer</div>
           <div>Our busses</div>
           <div>Book your ride</div>
         </div>
         <div>
-          <div>COL B</div>
-          <div>Home</div>
-          <div>What we offer</div>
-          <div>Our busses</div>
-          <div>Book your ride</div>
+          <div>Contact Information</div>
+          <div>Phone: 0434222343</div>
+          <div>Email: someone@candypartybus.com</div>
+          <div>Address: Serving Sydney and Surrounding Areas</div>
         </div>
         <div>
-          <div>COL C</div>
-          <div>Home</div>
-          <div>What we offer</div>
-          <div>Our busses</div>
-          <div>Book your ride</div>
+          <div>Business Hours</div>
+          <div>Mon - Sun</div>
+          <div style={{ display: "flex" }}>
+            <div>FB</div>
+            <div>Insta</div>
+            <div>Map</div>
+          </div>
         </div>
         <div>
-          <div>COL D</div>
-          <div>Home</div>
-          <div>What we offer</div>
-          <div>Our busses</div>
-          <div>Book your ride</div>
+          <div>Open 24 hours</div>
         </div>
       </div>
+      <div>LMAO DONT COPY US...grow up</div>
     </div>
   );
 };
