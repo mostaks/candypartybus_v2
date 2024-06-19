00cc18359f5458363a6cac2bb0b758224ca32bf3
Pat <pat.barilla@gmail.com>
2024-06-19 22:01:43 +1000

    added fonts, started working on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <div>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <div style={{ flexDirection: "row" }}>
+          <div>Quick Links</div>
+          <div>Home</div>
+          <div>What we offer</div>
+          <div>Our busses</div>
+          <div>Book your ride</div>
+        </div>
+        <div>
+          <div>COL B</div>
+          <div>Home</div>
+          <div>What we offer</div>
+          <div>Our busses</div>
+          <div>Book your ride</div>
+        </div>
+        <div>
+          <div>COL C</div>
+          <div>Home</div>
+          <div>What we offer</div>
+          <div>Our busses</div>
+          <div>Book your ride</div>
+        </div>
+        <div>
+          <div>COL D</div>
+          <div>Home</div>
+          <div>What we offer</div>
+          <div>Our busses</div>
+          <div>Book your ride</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
