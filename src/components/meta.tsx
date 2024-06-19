16cff9ae99df168161a9d5af308bb749c12af62a
Yianni Moustakas <yianni.m13@gmail.com>
2024-06-19 22:17:03 +1000

    V2 meta and favicons

---
+++ src/components/meta.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const meta = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+
+      <link rel="shortcut icon" type="image/jpg" href="LOGO.png" />
+      <title>Candy Party Bus</title>
+      <meta property="og:title" content="Candy Party Bus" key="title" />
+      <meta property="og:url" content="https://www.candypartybus.com.au" />
+      <meta
+        property="og:description"
+        content="Explore our fleet at Candy Party Bus in Sydney, NSW! From retro-themed to luxury coaches, find the perfect ride. Click here to view our buses!"
+      />
+    </>
+  );
+}
+
+export default meta;
