5328bc27bb25a7f08d2b2d5a4655ce9cf6edc0b4
Pat <pat.barilla@gmail.com>
2024-07-02 20:28:21 +1000

    divider done-ish

--- src/components/divider/index.tsx
+++ src/components/divider/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import styles from './divider.module.css';
+import React from "react";
+import styles from "./divider.module.css";
 
 const divider = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.background}>divider</div>
+      <div className={styles.weirdShape}>
+        <span className={styles.spanBox}>
+          At Candy Party Bus, we’re on a mission to redefine the party
+          experience. We envision an era where celebrations aren’t just events;
+          they're unforgettable adventures. Our goal is simple: to transport you
+          to an '80s-inspired wonderland on wheels while maintaining the highest
+          standards of professionalism.
+        </span>
+      </div>
+      <div className={styles.bottomRow}>bottom row</div>
     </div>
   );
-}
+};
 
-export default divider
+export default divider;
