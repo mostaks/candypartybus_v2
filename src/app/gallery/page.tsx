69205a688854e984fe8a2dbf5b5fe37c180bfdac
Yianni Moustakas <yianni.m13@gmail.com>
2024-08-02 16:36:11 +1000

    galleria

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import styles from "@/app/page.module.css";
 import Header from "@/components/header";
-import Hero from "@/components/hero";
 import Footer from "@/components/footer";
 import ReactImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -10,25 +9,27 @@ import "react-image-gallery/styles/css/image-gallery.css";
 let images = [];
 
 for (var i = 3; i <= 31; i++) {
-  images.push({
-    original: `/photos/${i}.jpg`,
-    thumbnail: `/photos/${i}.jpg`,
-  });
+    images.push({
+        original: `/photos/${i}.jpg`,
+        thumbnail: `/photos/${i}.jpg`,
+    });
 }
 
 const page = () => {
-  return (
-    <main className={styles.main}>
-      <Header />
-      <ReactImageGallery
-        items={images}
-        autoPlay={true}
-        slideInterval={10000}
-        showFullscreenButton={true}
-      />
-      <Footer />
-    </main>
-  );
+    return (
+        <main className={styles.main}>
+            <Header/>
+            <div style={{ position: 'relative', width: '100%', height: '100%', }}>
+                <ReactImageGallery
+                    items={images}
+                    autoPlay={true}
+                    slideInterval={10000}
+                    showFullscreenButton={true}
+                />
+            </div>
+            <Footer/>
+        </main>
+    );
 };
 
 export default page;
